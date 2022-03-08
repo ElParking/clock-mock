d4506166e1d4f0d059d91c49da382e1edf11c1ef
Malmi <jorge.malmierca@elparking.com>
2022-03-08 07:12:40 +0100

    absolute date with timezone fails

--- src/DateTimeMock/DateTimeMock.php
+++ src/DateTimeMock/DateTimeMock.php
@@ -19,7 +19,10 @@ class DateTimeMock extends \DateTime
 
         parent::__construct($datetime, $timezone);
 
-        if (\DateTime::createFromFormat('Y-m-d H:i:s', $datetime) || \DateTime::createFromFormat('Y/m/d H:i:s', $datetime)) {
+        if (\DateTime::createFromFormat('Y-m-d H:i:s', $datetime)
+            || \DateTime::createFromFormat('Y/m/d H:i:s', $datetime)
+            || \DateTime::createFromFormat('Y-m-d H:i', $datetime)
+        ) {
             return;
         }
 
